8bf9ab982c1a395a989a3b4c5df035987e6c2366
Mateus Ascacibas da Silva <mateus.asilva2001@hotmail.com>
2024-12-14 22:19:34 -0300

    First Commit with first spring project

---
+++ src/main/java/com/board/cyberboard/CyberboardSpringApplication.java
@@ -0,0 +1,13 @@
+package com.board.cyberboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberboardSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberboardSpringApplication.class, args);
+	}
+
+}
